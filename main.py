8cabd8e8c2385d14ab1a1cfe56905bfc25e507e1
Somraj Mondal <111075192+somrajmondal@users.noreply.github.com>
2022-09-20 09:08:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from instabot import *
+from instabot import Bot
 bot=Bot()
 log_in=bot.login((username='myselfsomraj',password=''))
 bot.unfollow('white_chilis')
 bot.follow('white_chilis')
 bot.send_massage(text='Hi i am somraj mondal',user_ids='white_chilis')
-bot.upload_photo()
+bot.upload_photo('20220216_144127.jpg',caption="This side Somraj")
+followers=bot.get_user_followers('myselfsomraj')
+for followerid in followers:
+  print(bot.get_user_followers(followerid))
